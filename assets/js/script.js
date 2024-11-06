1f571f15ee6aa24bc401a685e9e6d9deda53f464
PutriSaynata <saynataputri@gmail.com>
2024-11-06 18:18:34 +0700

    Update November 2024

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["GIS Analytics", "Geospatial Planning", "Spatial Data Science", "Web Development", "Android Development"],
+    strings: ["Software Engineer Enthusiast", "GIS Enthusiast", "Data Science Enthusiast"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
